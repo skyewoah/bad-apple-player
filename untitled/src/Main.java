f5cdde77301ea98bd4b1107f6244bbd8c51cc56c
skye <ninjamorn@gmail.com>
2024-11-02 15:47:56 -0400

    updated main.java to be better (not rly but just nicer for my own sake lol)

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -5,16 +5,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException {
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/home/skye/Documents/Image to Terminal/Audio.wav").getAbsoluteFile());
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("[insert audio wav location here]").getAbsoluteFile());
         Clip clip = AudioSystem.getClip();
         clip.open(audioInputStream);
         clip.start();
         System.out.print("\033[H\033[2J");
         for(int i = 0; i <= 6513; i++){
-            ImageProps image = new ImageProps("/home/skye/Documents/Image to Terminal/BadAppleInFrames/" + Integer.toString(i) +".png");
-            int maxX = image.getMaxX() / 6;
+            ImageProps image = new ImageProps("[insert frame folder here lol]" + Integer.toString(i) +".png");
+            int maxX = image.getMaxX() / 6; //factor by 6 to resize lol
             int maxY = image.getMaxY() / 6;
-            image.imageResize(maxX, maxY); // Resize to 800x600 pixels
+            image.imageResize(maxX, maxY);
             for(int y = 0; y < maxY; y++){
                 for(int x = 0; x < maxX; x++){
                     int white = image.getWhite(x, y);
@@ -27,7 +27,7 @@ public class Main {
                 System.out.println();
             }
             Thread.sleep(1000/24);
-            System.out.print("\033[H\033[2J");
+            System.out.print("\033[H\033[2J"); //clear
             System.out.flush();
         }
         clip.close();
